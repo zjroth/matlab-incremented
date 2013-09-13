692257fa231dc566000c864a14baec534b907e5e
zjroth <zjroth@gmail.com>
2013-09-13 15:09:07 -0500

    Update `ind2log` to output a matrix if requested

--- ind2log.m
+++ ind2log.m
@@ -1,5 +1,9 @@
-% vLogical = ind2log(vIndices, nLength)
-function vLogical = ind2log(vIndices, nLength)
-    vLogical = false(nLength, 1);
+% vLogical = ind2log(vIndices, vLengths)
+function vLogical = ind2log(vIndices, vLengths)
+    vLogical = false([vLengths, 1]);
+
+    assert(min(vIndices) > 0 && max(vIndices) <= numel(vLogical), ...
+           'ind2log: index out of bounds');
+
     vLogical(vIndices) = true;
 end
