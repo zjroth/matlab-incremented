66a5b27a6994e3d2a6824011167641a10c0483d5
Zachary Roth <rothz@pastalkova-ws2.janelia.priv>
2013-12-11 15:39:49 -0500

    Update `cellmap`
    Specifying the number of output arguments is now optional.

--- cellmap.m
+++ cellmap.m
@@ -1,7 +1,7 @@
 %
 % USAGE:
 %
-%    cellOut = cellmap(fcnMap, cellIn)
+%    cellOut = cellmap(fcnMap, <nArgOut>, cellIn1, ..., <cellInN>)
 %
 % DESCRIPTION:
 %
@@ -12,19 +12,46 @@
 %
 %    fcnMap
 %
-%       The function to apply to individual cells
+%       The function (as a handle) to apply to the entries of the supplied cells
 %
-%    cellIn
+%    nArgOut (default: `abs(nargout(fcnMap))`)
 %
-%       The input cell array whose cells the function should be applied to
+%       The number of requested output values from each invocation of `fcnMap`.
+%       To request all output values, try setting this to zero (though this
+%       may not work depending how MATLAB handles `varargout`).
+%
+%    cellIn1, ..., <cellInN>
+%
+%       The input cell arrays whose cells the function should be applied to.
+%       See `help cellfun` for more information.
 %
 % RETURNS:
 %
 %    cellOut
 %
-%       The ouput cell array whose entries are the output of the function.
-%       This is the same size as the input.
+%       Each entry of this is a cell array containing the list of output values
+%       for a single invocation of `fcnMap`.
 %
-function cellOut = cellmap(fcnMap, cellIn)
-    cellOut = cellfun(fcnMap, cellIn, 'UniformOutput', false);
+function cellOut = cellmap(fcnMap, varargin)
+    assert(~isempty(varargin), 'cellmap: no cell array provided');
+
+    if ~isa(varargin{1}, 'cell')
+        nArgOut = varargin{1};
+
+        if nArgOut == 0
+            nArgOut = nargout(fcnMap);
+        end
+
+        assert(isscalar(nArgOut) && isint(nArgOut), ...
+               'cellmap: invalid number of requested output values `nArgOut`');
+        cellInputCells = varargin(2:end);
+    else
+        nArgOut = nargout(fcnMap);
+        cellInputCells = varargin;
+    end
+
+    assert(nArgOut ~= 0, 'cellmap: supplied function outputs no values');
+
+    fcnApply = @(varargin) apply(fcnMap, abs(nArgOut), varargin{:});
+    cellOut = cellfun(fcnApply, cellInputCells{:}, 'UniformOutput', false);
 end
