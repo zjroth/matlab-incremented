5344073003661fcd5683781c238ee2f80ebc40e5
Zachary Roth <rothz@pastalkova-ws2.janelia.priv>
2013-12-19 23:02:46 -0500

    Add documentation for `gaussfilt`

--- gaussfilt.m
+++ gaussfilt.m
@@ -1,8 +1,34 @@
-function filter = gaussfilt(width, alpha)
+%
+% USAGE:
+%
+%    vFilter = gaussfilt(nWidth, <dNumStd>)
+%
+% DESCRIPTION:
+%
+%    Return a Gaussian filter; this is just a Gaussian window that has been
+%    normalized.
+%
+% ARGUMENTS:
+%
+%    nWidth
+%
+%       The total width of (i.e., number of indices in) the returned filter
+%
+%    dNumStd (default: 2.5)
+%
+%       The number of standard deviations to fit into the returned filter; this
+%       is the same as the `alpha` parameter from the function `gausswin`
+%
+% RETURN:
+%
+%    vFilter
+%
+%       The desired filter
+function vFilter = gaussfilt(nWidth, dNumStd)
     if nargin < 2
-        alpha = 2.5;
+        dNumStd = 2.5;
     end
 
-    filter = gausswin(width, alpha);
-    filter = filter / sum(filter);
+    vFilter = gausswin(nWidth, dNumStd);
+    vFilter = filter / sum(filter);
 end
