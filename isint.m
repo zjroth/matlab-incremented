49d6608ee1eeb6383ce948d6dea8d42741b30895
Zachary Roth <rothz@pastalkova-ws2.janelia.priv>
2013-12-19 23:14:29 -0500

    Add `isint` to test whether a number is an integer
    Matlabs built-in `isinteger` tests whether a variable is an integer
    type. This function is just to test whether a (floating-point) number
    is an inteter.

---
+++ isint.m
@@ -0,0 +1,12 @@
+% USAGE:
+%
+%    b = isint(x)
+%
+% DESCRIPTION:
+%
+%    Determine whether a (floating-point) number is an integer.
+%    This is equivalent to testing whether the floor of the number
+%    is equal to the number.
+function b = isint(x)
+    b = (floor(x) == x);
+end
