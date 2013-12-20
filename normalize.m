c3d32467b72c9e2003a0dfec3e1d430ce4152a61
Zachary Roth <rothz@pastalkova-ws2.janelia.priv>
2013-12-19 23:27:07 -0500

    Add `normalize`

---
+++ normalize.m
@@ -0,0 +1,16 @@
+% USAGE:
+%
+%    vOut = normalize(vIn, p)
+%
+% DESCRIPTION:
+%
+%    Normalize a vector
+function vOut = normalize(vIn, p)
+    assert(isvector(vIn), 'normalize: the input must be a vector');
+
+    if nargin < 2
+        p = 2;
+    end
+
+    vOut = vIn / norm(vIn, p);
+end
