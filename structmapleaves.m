884ccb225a48a4a1ae3dbec1162b1bf7c85f1982
Zachary Roth <rothz@pastalkova-ws2.janelia.priv>
2014-01-03 11:50:08 -0500

    Add documentation comment to `structmapleaves`

--- structmapleaves.m
+++ structmapleaves.m
@@ -1,3 +1,16 @@
+% USAGE:
+%    stctOut = structmapleaves(fcnMap, stctIn)
+%
+% DESCRIPTION:
+%    Apply a function to every leaf of a structure. A leaf in a (nested)
+%    structure is a descendant (a field, or a field of a field, etc.) that is
+%    not itself a structure.
+%
+% ARGUMENTS:
+%    fcnMap
+%       The function to apply to every leaf of the provided structure
+%    stctIn
+%       The structure
 function stctOut = structmapleaves(fcnMap, stctIn)
     stctOut = stctIn;
 
