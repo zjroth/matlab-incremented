d74865123be2647121e6b8197127e018069759cb
Zachary Roth <rothz@pastalkova-ws2.janelia.priv>
2014-01-09 02:10:34 -0500

    Add `mask` and `masksc`
    Currently, `masksc` always plots; but that is really just because I
    don't want to forget the proper incatation to plot with the
    appropriate color bar displayed.

---
+++ mask.m
@@ -0,0 +1,34 @@
+% USAGE:
+%    arrMasked = mask(arrImage, mtxMask, <vColor>)
+%
+% DESCRIPTION:
+%    Mask part of an image with a given color
+%
+% ARGUMENTS:
+%    arrImage
+%       The image to mask as a 3D array; see the "Tips" section of the
+%       documentation of `image` for more information
+%    mtxMask
+%       A logical matrix; this should contain `false` where the image is to
+%       be masked with color and `true` elsewhere.
+%    vColor (default: [1, 1, 1])
+%       The color to mask the image with as an RGB triple; defaults to white
+function arrMasked = mask(arrImage, mtxMask, vColor)
+    if nargin < 3
+        vColor = [1, 1, 1];
+    end
+
+    % Argument checking
+    assert(isvector(vColor) & length(vColor) == 3, ...
+           'mask: the provided color must be a vector of length 3');
+    assert(size(arrImage, 3) == 3, ...
+           'mask: the provided image must be m-by-n-by-3 array');
+    assert(isequal(size(arrImage), [size(mtxMask), 3]), ...
+           'mask: the image and mask must have the same size');
+
+    arrMasked = zeros(size(arrImage));
+    arrMask = repmat(mtxMask, 1, 1, 3);
+
+    arrMasked(arrMask) = arrImage(arrMask);
+    arrMasked(~arrMask) = repmat(vColor, nnz(~mtxMask), 1);
+end
---
+++ masksc.m
@@ -0,0 +1,56 @@
+% USAGE:
+%    [mtxMasked, vColorLimits] = masksc(mtxImage, mtxMask, vColor, ...
+%                                       vColorLimits, mtxColorMap)
+%
+% DESCRIPTION:
+%    Mask an image; similar to `imagesc`
+%
+% ARGUMENTS:
+%    mtxImage
+%       The image to mask as a matrix
+%    mtxMask
+%       A logical matrix; this should contain `false` where the image is to
+%       be masked with color and `true` elsewhere.
+%    vColor (default: [1, 1, 1])
+%       The color to mask the image with as an RGB triple; defaults to white
+%    vColorLimits (default: max and min in displayed part of `mtxImage`)
+%       A vector of length 2; the minimum and maximum values of the color map
+%    mtxColorMap (default: `jet(64)`)
+%       A 3-column matrix containing RGB triples
+function [mtxMasked, vColorLimits] = masksc(mtxImage, mtxMask, vColor, ...
+                                            vColorLimits, mtxColorMap)
+    % Argument checking
+    assert(isequal(size(mtxImage), size(mtxMask)), ...
+           'maskimage: the image and mask must have the same size');
+
+    if nargin < 4 || isempty(vColorLimits)
+        vMaskedVals = mtxImage(mtxMask);
+        vColorLimits = [min(vMaskedVals), max(vMaskedVals)];
+    end
+
+    if nargin < 5
+        mtxColorMap = jet(64);
+    end
+
+    % Clamp the image to the given range.
+    mtxClamped = (mtxImage - vColorLimits(1)) / diff(vColorLimits);
+    mtxClamped(mtxClamped < 0) = 0;
+    mtxClamped(mtxClamped >= 1) = 1 - eps;
+
+    % Map the clamped values to row indices of the color map, and then build
+    % the image from that information.
+    mtxColorIndices = floor(mtxClamped * size(mtxColorMap, 1)) + 1;
+    arrImage = reshape(mtxColorMap(mtxColorIndices(:), :), [size(mtxImage), 3]);
+
+    % Now simply mask the image array.
+    if nargin < 3 || isempty(vColor);
+        mtxMasked = mask(arrImage, mtxMask);
+    else
+        mtxMasked = mask(arrImage, mtxMask, vColor);
+    end
+
+    % Plot it.
+    image(mtxMasked, 'CDataMapping', 'scaled');
+    caxis(vColorLimits);
+    colorbar();
+end
