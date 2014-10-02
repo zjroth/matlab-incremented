7a26f59fc213341d82e618e523d8615a0e7cbde7
Zachary Roth <rothz@pastalkova-ws2.janelia.priv>
2014-10-02 17:43:02 -0400

    Update `findfiles`

--- findfiles.m
+++ findfiles.m
@@ -1,37 +1,34 @@
-%
 % USAGE:
-%
 %    cellFiles = findfiles(strDir, strRegexp)
 %
 % DESCRIPTION:
-%
 %    Find all of the files in the given directory that match the given regexp
 %
 % ARGUMENTS:
-%
 %    strDir
-%
 %       The directory to search
-%
 %    strRegexp
-%
 %       The regular expression to search for in file names
 %
 % RETURNS:
-%
 %    cellFiles
-%
 %       A cell array containing the names of the files that match the given
 %       regular expression
-%
 function cellFiles = findfiles(strDir, strRegexp)
+    % Default to matching all files...
+    if nargin < 2
+        strRegexp = '.*';
+    end
+
+    % ...in the current directory.
+    if nargin < 1
+        strDir = '.';
+    end
+
     % Get the directory listing.
     stctDirectory = dir(strDir);
 
-    % Get the file names that match the given regular expression.
-    vMatching = cellfun(@(x) ~isempty(regexp(x, strRegexp)), ...
-                        {stctDirectory.name});
-
-    % Retrieve the names, and return them as a cell array.
-    cellFiles = {stctDirectory(vMatching).name};
+    % Keep only those file names that match the given regular expression.
+    fcnMatches = @(s) regexptest(s, strRegexp);
+    cellFiles = extract(fcnMatches, {stctDirectory.name});
 end
