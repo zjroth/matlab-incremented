d7cf0c498293de1f2aef2b070f35727a48f8ea02
Zachary Roth <rothz@pastalkova-ws2.janelia.priv>
2014-10-03 20:37:44 -0400

    Update `cellfcn`
    A conditional was checking the number of outputs of the supplied
    function, which was causing an error when a class method was passed. If
    someone wants to pass a function with no output, they should use
    `cellfun` instead.

--- cellfcn.m
+++ cellfcn.m
@@ -6,10 +6,6 @@
 %
 % See also CELLFUN
 function varargout = cellfcn(fcn, varargin)
-    if nargout(fcn) == 0
-        cellfun(fcn, varargin{:});
-    else
-        varargout = cell(1, max(1, nargout()));
-        [varargout{:}] = cellfun(fcn, varargin{:}, 'UniformOutput', false);
-    end
+    varargout = cell(1, max(1, nargout()));
+    [varargout{:}] = cellfun(fcn, varargin{:}, 'UniformOutput', false);
 end
